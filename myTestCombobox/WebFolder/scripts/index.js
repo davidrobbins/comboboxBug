4d43535c8495e7fe7284d351ebc407dccb8eaf92
David Robbins <dave@wakanda.org>
2012-08-30 15:14:58 -0700

    combobox bug

---
+++ myTestCombobox/WebFolder/scripts/index.js
@@ -0,0 +1,37 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button2 = {};	// @button
+	var button1 = {};	// @button
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button2.click = function button2_click (event)// @startlock
+	{// @endlock
+		$$('container1').hide();
+	};// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		$$('container1').show();
+	};// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		testArray = [];
+		testArray.push({title: ''});
+		testArray.push({title: 'one'});
+		testArray.push({title: 'two'});
+		testArray.push({title: 'three'});
+		WAF.sources.testArray.sync();
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button2", "click", button2.click, "WAF");
+	WAF.addListener("button1", "click", button1.click, "WAF");
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
